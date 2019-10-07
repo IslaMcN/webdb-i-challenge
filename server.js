9ff4a83dbcd68c8487abee077f0398e84c2cdac7
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-10-07 13:06:12 -0700

    Done I think

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const db = require('./data/dbConfig.js');
-
+const BudgetRouter = require('./data/budget/budgetrouter')
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/budget', db);
+server.use('/api/budget', BudgetRouter);
 
 server.get('/', (req, res) => {
     res.send('<h4> This is working, this is woooorking.</h4>')
