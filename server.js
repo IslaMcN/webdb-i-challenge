1297f4e166638da19113b0386667493746dfe97a
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-10-07 12:16:05 -0700

    server done

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ const server = express();
 
 server.use(express.json());
 
+server.use('/api/budget', db);
+
+server.get('/', (req, res) => {
+    res.send('<h4> This is working, this is woooorking.</h4>')
+})
+
 module.exports = server;
